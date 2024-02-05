1ac7fb30749fcdcdbde031d62d6cf2e3f3d6cb36
Rizky P. Mahendra <rizkymahendra2346@gmail.com>
2024-02-05 15:14:25 +0700

    Added styling boilerplate

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { Plus_Jakarta_Sans } from 'next/font/google';
 import './globals.css';
+import { cn } from '@/lib/utils';
+import { Toaster } from 'sonner';
 
 const jakarta_sans = Plus_Jakarta_Sans({ subsets: ['latin'] });
 
@@ -16,8 +18,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <body className={jakarta_sans.className}>{children}</body>
+    <html lang='en' className='h-full'>
+      <body
+        className={cn('relative h-full antialiased', jakarta_sans.className)}
+      >
+        <main className='relative flex flex-col min-h-screen'>
+          <div className='flex-grow flex-1'>{children}</div>
+        </main>
+
+        <Toaster position='top-center' richColors />
+      </body>
     </html>
   );
 }
